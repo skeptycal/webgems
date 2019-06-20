539a82f654d745b975608d82a975c6af914c5a1a
skeptycal <skeptycal@gmail.com>
2019-06-19 22:39:53 -0500

    js formatting

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,67 +5,107 @@ export default {
   mode: 'universal',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'webgems',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
-      { hid: 'keywords', name: 'keywords', content: 'css, html, php, server, resources, design, gems, nuxt, javascript, tutorials, development, software'},
-      { name:'theme-color', content: '#ffffff' },
-      { name: 'msapplication-TileColor', content: '#da532c' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-      { rel:'manifest', href:'/site.webmanifest' },
-      { rel:'icon', type:'image/png', sizes:'16x16', href:'/favicon-16x16.png' },
-      { rel:'icon', type:'image/png', sizes:'32x32', href:'/favicon-32x32.png' },
-      { rel:'apple-touch-icon', sizes:'76x76', href:'/apple-touch-icon.png' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'css, html, php, server, resources, design, gems, nuxt, javascript, tutorials, development, software'
+      },
+      {
+        name: 'theme-color',
+        content: '#ffffff'
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#da532c'
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#5bbad5'
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/apple-touch-icon.png'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   /*
-  ** Generate dynamic routes
-  */
+   ** Generate dynamic routes
+   */
   generate: {
     routes: store.map(category => category.slug)
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     'nuxt-clipboard2',
   ],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
